5d0c5f8949623a70b804ced1b3e43ed61e8dd747
Zachary Malinka <zacharymalinka@gmail.com>
2025-02-07 20:31:09 -0500

    api testing. will replace src folder next

--- frontend/components/Login/LoginSlice.js
+++ frontend/components/Login/LoginSlice.js
@@ -3,7 +3,8 @@ import api from "../../store/api";
 const loginApi = api.injectEndpoints({
   endpoints: (builder) => ({
     addLogin: builder.mutation({
-      query: ({ first_name, last_name, email, password }) => ({
+      //I got rid of "first_name, last_name" in query because I don't think it's necessary
+      query: ({ email, password }) => ({
         url: "api/users/login",
         method: "POST",
         body: { email, password },
--- frontend/store/api.js
+++ frontend/store/api.js
@@ -1,9 +1,23 @@
 // this is where you put the database I'm assuming
 
-import { createApi } from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const api = createApi({
   reducerPath: "apiOne",
+  baseQuery: fetchBaseQuery({
+    baseUrl:
+      "postgresql://bridge_qg3l_user:hn2XrSnQEePgpLfS8QztgF5T59ss0XxX@dpg-cui0g5dds78s73drk4ug-a.oregon-postgres.render.com/bridge_qg3l",
+
+    prepareHeaders: (headers) => {
+      const token = sessionStorage.getItem("token");
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["User"], //let's see if this messes anything up
+  endpoints: () => ({}),
 });
 
 export default api;

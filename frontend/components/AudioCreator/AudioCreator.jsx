4f95f31f156655109d798caf455eb4a456002679
Zachary Malinka <zacharymalinka@gmail.com>
2025-03-11 15:27:20 -0400

    Uses sessionStorage instead of rtk persist. State keeps until tab is close. Also created an about me page.

--- frontend/components/AudioCreator/AudioCreator.jsx
+++ frontend/components/AudioCreator/AudioCreator.jsx
@@ -5,7 +5,12 @@ export default function AudioCreator() {
   return (
     <>
       <button onClick={triggerBackend}>Start</button>
+      <div>
       <Link to="/audio">Published Audio</Link>
+      </div>
+      <div>
+      <Link to="/singleUser">Your Account</Link>
+      </div>
     </>
   );
 }

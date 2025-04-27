e1cb3ee049f59a5258c2e68ba17804a3c0371448
Zachary Malinka <zacharymalinka@gmail.com>
2025-04-26 21:47:12 -0400

    Can like and dislike comments with reaction updates, as well as real_time global page refreshing on user events.

--- frontend/components/Registration/Registration.jsx
+++ frontend/components/Registration/Registration.jsx
@@ -18,9 +18,9 @@ export default function Registration({ setMe }) {
         username,
         email,
         normal_password,
-      });
-      setMe(response.data.me);
-      sessionStorage.setItem("token", response.data.token);
+      }).unwrap();
+      setMe(response.me);
+      sessionStorage.setItem("token", response.token);
       navigate("/audioCreator");
     } catch (error) {
       console.error(error);
@@ -73,7 +73,7 @@ export default function Registration({ setMe }) {
               </label>
             </div>
             <button className="button">Register</button>
-            {isLoading && <output className="text">Creating user...</output>}
+            {isLoading && <output className="error">Creating user...</output>}
             {error && (
               <output className="text">
                 Error creating user {error.message}

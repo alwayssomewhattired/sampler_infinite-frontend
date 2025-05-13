0a64aa22126bb12510866c67d08c4d3babc4b6c9
Zachary Malinka <zacharymalinka@gmail.com>
2025-05-12 23:29:26 -0400

    All components except SingleUser and GranularInfinite are styled. Styles are now organized into style.css and I modularized the html layout

---
+++ frontend/components/Layout/Account.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import "./../../styles/styles.css";
+
+export default function Account({ me }) {
+  return (
+    <div className="account menu neu">
+      <h2 className="li-header">Account</h2>
+      <ul>
+        {me ? (
+          <li>
+            <Link className="neu" to="/singleUser">
+              My Account
+            </Link>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link className="neu" to="/login">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link className="neu" to="/register">
+                Register
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </div>
+  );
+}

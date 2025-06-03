e7e60b6b651e46b8c5e51dbc8233d2c5b12721a6
Zachary Malinka <zacharymalinka@gmail.com>
2025-06-03 12:27:30 -0400

    tweaks

--- git-auto-pull.sh
+++ git-auto-pull.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-cd ./sampler_infinite-frontend || exit
+#  AUTOMATED CODE PULLER
+
+set -ex
+
+
+cd /home/ubuntu/sampler_infinite-frontend || {
+ echo "Failed to cd into repo directory"
+ exit 1
+}
+
+echo "Current directory: $(pwd)"
+echo "Running git pull..."
 
 REMOTE=prod
 BRANCH=main
@@ -11,9 +22,27 @@ while true; do
     LOCAL_HASH=$(git rev-parse $BRANCH)
     REMOTE_HASH=$(git rev-parse $REMOTE/$BRANCH)
 
-    if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
+    echo "Local hash: $LOCAL_HASH"
+    echo "Remote hash: $REMOTE_HASH"
+
+        if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
         echo "[$(date)] New commit detected. Pulling..."
-        git pull $REMOTE $BRANCH
+        git pull --ff-only $REMOTE $BRANCH
+
+        cd frontend || {
+            echo "Failed to cd into frontend"
+            exit 1
+        }
+
+        echo "Installing dependencies..."
+        npm install
+
+        echo "Running build..."
+        npm run build
+
+        echo "Finished!"
+
+        cd ..
     fi
 
     sleep 60

6a764c4bc1e5f661c8bfedd8fffdca318e150e22
Zachary Malinka <zacharymalinka@gmail.com>
2025-06-10 22:25:15 -0400

    Still refactoring AudioCreator

--- frontend/hooks/useWebSocket.js
+++ frontend/hooks/useWebSocket.js
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react";
 import { useDefaultUser } from "./useDefaultUser";
 
+// TODO
+// 1. SEND USER_ID IN EVERY WEBSOCKET MESSAGE. THAT WAY I CAN TRACK THE SPECIFIC CONNECTION ID ON MY WEBSOCKET SERVER IN ORDER TO SEND BACK DATA.
+// 2. SEND AUDIO FILE BACK FROM C++ TO HERE.
+// 3.
+// 4. IMPORTANT!!!!!! >>>>  MAKE SURE TO CHECK 'ME' FOR LOGGED IN USER ID. IF !ME, THEN USE DEFAULT_USER ID.
+
 export const useWebSocket = (url, { onOpen, onMessage, onError, onClose }) => {
   const [socket, setSocket] = useState(null);
   const [connected, setConnected] = useState(false);
@@ -29,6 +35,7 @@ export const useWebSocket = (url, { onOpen, onMessage, onError, onClose }) => {
       console.log("Received:", event.data);
       try {
         const message = JSON.parse(event.data);
+        console.log("jsonparsed message: ", message)
         onMessage?.(message, ws);
       } catch (error) {
         console.error(error);

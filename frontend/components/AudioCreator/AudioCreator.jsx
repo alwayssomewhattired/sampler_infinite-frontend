93dd7bdfb7131f0f2a2bc2c6c28b170cc2257d3a
Zachary Malinka <zacharymalinka@gmail.com>
2025-06-22 20:39:57 -0400

    testing

--- frontend/components/AudioCreator/AudioCreator.jsx
+++ frontend/components/AudioCreator/AudioCreator.jsx
@@ -16,7 +16,7 @@ export default function AudioCreator({ setNewAudio, newAudio, me }) {
   const [connected, setConnected] = useState(false);
 
   const [selectNote, setSelectNote] = useState("");
-// litrally nothin
+// executable tester :'=
   const [whileLoading, setWhileLoading] = useState(false);
   const navigate = useNavigate();
 

0a34e8956f4e694a94516670eda80d76e3b218ef
Zachary Malinka <zacharymalinka@gmail.com>
2025-06-22 20:11:25 -0400

    updated git pull

--- git-auto-pull.sh
+++ git-auto-pull.sh
@@ -15,19 +15,20 @@ echo "Running git pull..."
 
 REMOTE=prod
 BRANCH=main
+REMOTE_BRANCH="$REMOTE/$BRANCH"
 
 while true; do
-    git fetch $REMOTE $BRANCH
+    git fetch $REMOTE 
 
     LOCAL_HASH=$(git rev-parse $BRANCH)
-    REMOTE_HASH=$(git rev-parse $REMOTE/$BRANCH)
+    REMOTE_HASH=$(git rev-parse $REMOTE_BRANCH)
 
     echo "Local hash: $LOCAL_HASH"
     echo "Remote hash: $REMOTE_HASH"
 
         if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
         echo "[$(date)] New commit detected. Pulling..."
-        git pull --ff-only $REMOTE $BRANCH
+        git reset --hard $REMOTE_BRANCH
 
         cd frontend || {
             echo "Failed to cd into frontend"
